b8513539a7cb559c1599ef01cd2ac06f55cb4df7
Json Alzate <developer.alzate@gmail.com>
2023-01-29 11:31:28 -0500

    🚧 Player - bug sync animation after resume

--- src/app/home/components/breathe/breathe.component.ts
+++ src/app/home/components/breathe/breathe.component.ts
@@ -11,6 +11,10 @@ import { NavController } from '@ionic/angular';
 })
 export class BreatheComponent implements OnInit {
 
+  title = 'Respiración';
+  timer = '00:00';
+  timeRemaining = 0;
+
   // descripción del id para la rutina de respiración
   /*
   * 0: 4-6
@@ -20,6 +24,8 @@ export class BreatheComponent implements OnInit {
   classBreathe = '';
   classBreatheBackground = '';
   status: 'exhale' | 'inhale' | 'hold' = 'inhale';
+  cicleSeconds = 0;
+  statusPlayer: 'play' | 'pause' = 'play';
 
   // se utiliza para saber cuanto tiempo le queda al estado actual de la respiración
   // util para cuando se pause por ejemplo
@@ -30,6 +36,7 @@ export class BreatheComponent implements OnInit {
   cicleStatus: string[] = [];
 
   intervalObject: any;
+  timerInterval: any;
 
 
   constructor(
@@ -39,12 +46,22 @@ export class BreatheComponent implements OnInit {
     private navController: NavController
   ) {
     const id = this.activatedRoute.snapshot.paramMap.get('id');
+    const timer = this.activatedRoute.snapshot.paramMap.get('timer');
+    // se calcula el tiempo restante en segundos
+    this.timeRemaining = parseInt(timer, 10) * 60;
+    this.timer = this.getTimer();
+
+    // se inicia el timer
+    this.startTimer();
+
     switch (id) {
       case '0':
+        this.title = 'Relajación';
         this.classBreathe = 'breathe-4-6';
         this.classBreatheBackground = 'breathe-4-6-background';
         this.cicle = [4, 6];
         this.cicleStatus = ['inhale', 'exhale'];
+        this.status = this.cicleStatus[this.cicleIndex] as any;
         this.startBreathe();
         break;
     }
@@ -52,24 +69,30 @@ export class BreatheComponent implements OnInit {
   }
 
   ngOnInit() {
-
   }
 
 
-  startBreathe() {
-    const cicleSeconds = this.cicle[this.cicleIndex];
 
-    // modifica la velocidad de la animación para que coincida con el tiempo del estado actual
-    this.setTimingAnimation(cicleSeconds);
 
-    this.counterInterval = cicleSeconds;
-    this.status = this.cicleStatus[this.cicleIndex] as any;
+  startBreathe(isResume?: boolean) {
+
+    if (!isResume) {
+      this.cicleSeconds = this.cicle[this.cicleIndex];
+      // modifica la velocidad de la animación para que coincida con el tiempo del estado actual
+      this.setTimingAnimation(this.cicleSeconds);
+      this.counterInterval = this.cicleSeconds;
+    }
+
 
     this.intervalObject = setInterval(() => {
+
+      if (isResume) {
+
+        this.resumeAnimation();
+      }
       this.counterInterval--;
       console.log(this.counterInterval);
-
-      if (this.counterInterval === 0) {
+      if (this.counterInterval <= 0) {
         this.nextStep();
       }
     }
@@ -77,16 +100,30 @@ export class BreatheComponent implements OnInit {
 
   }
 
+
+  // al reanudar
+  onResume() {
+    console.log('resume ', this.counterInterval);
+    this.statusPlayer = 'play';
+    this.startBreathe(true);
+    this.startTimer();
+  }
+
+
+
   // pasa al siguiente estado de la respiración
   nextStep() {
-
     // clear interval
-    clearInterval(this.intervalObject);
+    if (this.intervalObject) {
+      clearInterval(this.intervalObject);
+    }
 
     this.cicleIndex++;
     if (this.cicleIndex === this.cicle.length) {
       this.cicleIndex = 0;
     }
+    this.status = this.cicleStatus[this.cicleIndex] as any;
+    console.log('status', this.status);
 
     this.startBreathe();
   }
@@ -95,6 +132,76 @@ export class BreatheComponent implements OnInit {
   setTimingAnimation(seconds: number) {
     const rootElement = this.elementRef.nativeElement;
     this.renderer.setAttribute(rootElement, 'style', '--timing: ' + seconds + 's;');
+  }
+
+
+
+  // timer
+  // se calcula el tiempo restante en minutos y segundos
+  getTimer() {
+    const minutes = Math.floor(this.timeRemaining / 60);
+    const seconds = this.timeRemaining - minutes * 60;
+    return minutes + ':' + seconds;
+  }
+
+  // se construye el timer en base al tiempo restante
+  startTimer() {
+    this.timerInterval = setInterval(() => {
+      this.timeRemaining--;
+      this.timer = this.getTimer();
+
+      if (this.timeRemaining === 0) {
+        console.log('termino');
+      }
+    }, 1000);
+  }
+
+  // al pausar
+  onPuase() {
+    console.log('pause ', this.counterInterval);
+    this.statusPlayer = 'pause';
+    clearInterval(this.intervalObject);
+    clearInterval(this.timerInterval);
+    this.pauseAnimation();
+  }
+
+
+
+  pauseAnimation() {
+    // se adiciona la clase para que se detenga la animación
+    const element = this.elementRef.nativeElement.querySelector('.pointer-breathe-container');
+    if (element) {
+      element.style.animationPlayState = 'paused';
+    }
+
+    const element2 = this.elementRef.nativeElement.querySelector('.breathe-animation.breathe-in');
+    if (element2) {
+      element2.style.animationPlayState = 'paused';
+    }
+    const element3 = this.elementRef.nativeElement.querySelector('.breathe-animation.breathe-out');
+    if (element3) {
+      element3.style.animationPlayState = 'paused';
+    }
+  }
+
+  resumeAnimation() {
+
+
+    const element = this.elementRef.nativeElement.querySelector('.pointer-breathe-container');
+    // this.renderer.removeClass(element, 'pause-animation');
+    if (element) {
+      element.style.animationPlayState = 'running';
+    }
+
+
+    const element2 = this.elementRef.nativeElement.querySelector('.breathe-animation.breathe-in');
+    if (element2) {
+      element2.style.animationPlayState = 'running';
+    }
+    const element3 = this.elementRef.nativeElement.querySelector('.breathe-animation.breathe-out');
+    if (element3) {
+      element3.style.animationPlayState = 'running';
+    }
   }
 
 
